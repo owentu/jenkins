39e00fa9147b0db26d7bd4a9cb03ae9f44c97736
DESKTOP-FPCR4BL\Owen <owen.tu@ixorp.com>
2017-10-20 11:34:33 +0800

    commit

---
+++ jenkins/src/com/ixorp/jenkins/si/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ixorp.jenkins.si;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
