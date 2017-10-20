3f64a5fca1ae5ba0078391116156ae4350667a84
DESKTOP-FPCR4BL\Owen <owen.tu@ixorp.com>
2017-10-20 14:52:32 +0800

    no message

--- EDISERVICE/com/ixorp/edi/si/GenerateSI.java
+++ EDISERVICE/com/ixorp/edi/si/GenerateSI.java
@@ -1,10 +1,20 @@
 package com.ixorp.edi.si;
 
+
+import com.alibaba.fastjson.JSONObject;
 public class GenerateSI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("SI");
+		
+		JSONObject obj = new JSONObject();
+		
+		obj.put("name", "owen");
+		obj.put("tel", "25002636");
+		
+		
+		System.out.println(obj.toString());
 	}
 
 }
